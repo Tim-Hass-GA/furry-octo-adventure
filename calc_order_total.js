6a4c1e29791badc7c07582e8bd5b83fb66d3e90a
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-06-06 10:47:24 -0700

    initial commit - testing examples from scratch, no test runners - install quokka

---
+++ calc_order_total.js
@@ -0,0 +1,9 @@
+const orderTotal =  (order) => {
+  const totalNormalItems =
+    order.totalNormalItems
+    .filter( x => !x.shipping )
+    .reduce( (prev, cur) => prev + cur.quantity * cur.price, 0)
+  const shippingItem = order.items.find( x => !!x.shipping )
+  const shipping = totalNormalItems > 1000 ? 0 : shippingItem.price
+  return totalNormalItems
+}
